b7cdb6551559e460ed2c8184c6ea95fe0de58fe4
kirbythomson <kirby.thomson@riotinto.com>
2025-02-10 16:40:02 +1000

    Updated make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import os
 from pathlib import Path
-# Removed pyodbc import as it's no longer needed
-# from config import user_name_odp, password_odp
-
-# Removed get_crew_data and get_shift_data functions
 
 def load_data():
     crew_change_over = pd.read_csv('src/data/raw/crew_change_over.csv')
